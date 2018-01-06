42ee1cc66a432c11269decf80b7356879cb993b1
podhmo <ababjam61+github@gmail.com>
2018-01-06 22:59:20 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-
 import os
 import sys
 import fastentrypoints
@@ -14,45 +13,41 @@ try:
 except IOError:
     README = CHANGES = ''
 
-
 install_requires = [
     'yapf',
 ]
 
+docs_extras = []
 
-docs_extras = [
-]
+tests_require = []
 
-tests_require = [
-]
+testing_extras = tests_require + []
 
-testing_extras = tests_require + [
-]
-
-setup(name='yayapf',
-      version='0.0.1',
-      description='yet another yapf',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: Implementation :: CPython",
-      ],
-      keywords='yapf, style format, pep8',
-      author="podhmo",
-      author_email="ababjam61+github.com",
-      url="https://github.com/yayapf",
-      packages=find_packages(exclude=["yayapf.tests"]),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      extras_require={
-          'testing': testing_extras,
-          'docs': docs_extras,
-      },
-      tests_require=tests_require,
-      test_suite="yayapf.tests",
-      entry_points="""
+setup(
+    name='po-yayapf',
+    version='0.0.1',
+    description='yet another individual yapf',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords='yapf, style format, pep8',
+    author="podhmo",
+    author_email="ababjam61+github.com",
+    url="https://github.com/yayapf",
+    packages=find_packages(exclude=["yayapf.tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+        'docs': docs_extras,
+    },
+    tests_require=tests_require,
+    test_suite="yayapf.tests",
+    entry_points="""
       [console_scripts]
       yayapf=yayapf:main
-""")
-
+"""
+)
